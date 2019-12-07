5b2eee8fdde2a2e4c37acd6f13659ace04c36d24
Cody Clark <codyclark2119@gmail.com>
2019-12-07 16:39:51 -0600

    redux added and auth functionality working

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react';
 import {
   Navbar,
   Nav,
@@ -8,77 +8,104 @@ import {
   Container,
   InputGroup,
   Dropdown
-} from "react-bootstrap";
+} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-function NavBar(props) {
+const NavBar = ({ auth: { isAuthenticated, loading }, logout }, props) => {
+  const authLinks = (
+    <Navbar.Collapse id='basic-navbar-nav'>
+      <Nav className='mr-auto'>
+        <Nav.Link href='/'>Shop</Nav.Link>
+        <Nav.Link href='/dashboard'>Dashboard</Nav.Link>
+        <Nav.Link onClick={logout}>Logout</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  );
+
+  const guestLinks = (
+    <Navbar.Collapse id='basic-navbar-nav'>
+      <Nav className='mr-auto'>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <Nav.Link href='/signup'>Signup</Nav.Link>
+        <Nav.Link href='/login'>Login</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  );
   return (
-    <Navbar fixed="top" expand="md" bg="dark" variant="dark">
+    <Navbar fixed='top' expand='md' bg='dark' variant='dark'>
       <Container>
-        <Navbar.Brand href="/">Fakazon</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href='/'>Fakazon</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <InputGroup>
           <DropdownButton
             as={InputGroup.Prepend}
-            variant="outline-secondary"
-            title="Categories"
-            id="input-group-dropdown-1"
+            variant='outline-secondary'
+            title='Categories'
+            id='input-group-dropdown-1'
           >
             <Dropdown.Item
-              name="category"
-              value="books"
+              name='category'
+              value='books'
               onClick={event => props.categorySearch(event)}
-              as="button"
-              type="submit"
+              as='button'
+              type='submit'
             >
               Books
             </Dropdown.Item>
             <Dropdown.Item
-              name="category"
-              value="video games"
+              name='category'
+              value='video games'
               onClick={event => props.categorySearch(event)}
-              as="button"
+              as='button'
             >
               Video Games
             </Dropdown.Item>
             <Dropdown.Item
-              name="category"
-              value="electronics"
+              name='category'
+              value='electronics'
               onClick={event => props.categorySearch(event)}
-              as="button"
+              as='button'
             >
               Electronics
             </Dropdown.Item>
             <Dropdown.Item
-              name="category"
-              value="clothes"
+              name='category'
+              value='clothes'
               onClick={event => props.categorySearch(event)}
-              as="button"
+              as='button'
             >
               Clothes
             </Dropdown.Item>
           </DropdownButton>
           <FormControl
-            name="userQuery"
+            name='userQuery'
             value={props.userQuery}
             onChange={props.handleInputChange}
-            aria-describedby="basic-addon1"
+            aria-describedby='basic-addon1'
           />
           <InputGroup.Append>
-            <Button variant="outline-secondary" onClick={props.userSearch}>
+            <Button variant='outline-secondary' onClick={props.userSearch}>
               Search
             </Button>
           </InputGroup.Append>
         </InputGroup>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
       </Container>
     </Navbar>
   );
-}
+};
 
-export default NavBar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(NavBar);
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,111 +1,108 @@
-import React from "react";
-import { Col, Row, Container, Jumbotron, Form, Button } from "react-bootstrap";
-import API from "../utils/API";
+import React, { useState } from 'react';
+import { Col, Row, Container, Jumbotron, Form, Button } from 'react-bootstrap';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { register } from '../actions/auth';
 
-class Signup extends React.Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    password2: ""
+const Register = ({ register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { username, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    register(username, email, password);
   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (
-      this.state.password &&
-      this.state.password2 &&
-      this.state.username &&
-      this.state.email
-    ) {
-      if (this.state.password === this.state.password2) {
-        API.signUp({
-          username: this.state.username,
-          email: this.state.email,
-          password: this.state.password
-        })
-          .then(res => console.log(res))
-          .catch(err => console.log(err));
-      }
-    }
-  };
-
-  render() {
-    return (
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col md="8">
-            <Jumbotron>
-              <Form>
-                <Form.Group controlId="formBasicUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    value={this.state.username}
-                    onChange={this.handleInputChange}
-                    name="username"
-                    type="input"
-                    placeholder="Enter Username"
-                  />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    name="email"
-                    type="input"
-                    placeholder="Enter email"
-                  />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                  />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword2">
-                  <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    value={this.state.password2}
-                    onChange={this.handleInputChange}
-                    name="password2"
-                    type="password"
-                    placeholder="Password"
-                  />
-                </Form.Group>
-                <Button
-                  disabled={!(this.state.email && this.state.password)}
-                  onClick={this.handleFormSubmit}
-                  variant="primary"
-                  type="submit"
-                >
-                  Submit
-                </Button>
-                <Form.Text className="text-muted">
-                  Have an account? <a href="/login">Log In!</a>
-                </Form.Text>
-              </Form>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    );
+  if (isAuthenticated) {
+    return <Redirect to='/' />;
   }
-}
 
-export default Signup;
+  return (
+    <Container>
+      <Row className='justify-content-md-center'>
+        <Col md='8'>
+          <Jumbotron>
+            <Form>
+              <Form.Group controlId='formBasicUsername'>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  value={username}
+                  onChange={e => onChange(e)}
+                  name='username'
+                  type='input'
+                  placeholder='Enter Username'
+                />
+              </Form.Group>
+              <Form.Group controlId='formBasicEmail'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  value={email}
+                  onChange={e => onChange(e)}
+                  name='email'
+                  type='input'
+                  placeholder='Enter email'
+                />
+                <Form.Text className='text-muted'>
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId='formBasicPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={password}
+                  onChange={e => onChange(e)}
+                  name='password'
+                  type='password'
+                  placeholder='Password'
+                />
+              </Form.Group>
+              <Form.Group controlId='formBasicPassword2'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  value={password2}
+                  onChange={e => onChange(e)}
+                  name='password2'
+                  type='password'
+                  placeholder='Password'
+                />
+              </Form.Group>
+              <Button
+                disabled={!(password === password2)}
+                onClick={e => onSubmit(e)}
+                variant='primary'
+                type='submit'
+              >
+                Submit
+              </Button>
+              <Form.Text className='text-muted'>
+                Have an account? <Link to='/login'>Log In!</Link>
+              </Form.Text>
+            </Form>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+Register.propTypes = {
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { register })(Register);
